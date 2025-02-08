db68d4e447425ca66bbf248fdffb299afc9fd933
clmateus <cl.mateuscosta@gmail.com>
2025-02-07 23:33:45 -0300

    added exercicio2.js

---
+++ exercicio2.js
@@ -0,0 +1,8 @@
+const nome = "Mateus"
+const sobrenome = "Costa"
+let idade = 24
+let peso = 82.5
+let altura = 1.72
+let imc = peso / (altura * 2)
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg, tem ${altura}m de altura e seu IMC é de ${imc}`)
