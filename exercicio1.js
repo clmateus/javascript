eaf62ce867dc40eaae9134b2f8e133edc39bc13b
clmateus <cl.mateuscosta@gmail.com>
2025-02-07 23:14:59 -0300

    exercicio1.js updated

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,2 @@
 // Exercício 1 - console.log
-console.log('Meu nome é Mateus. Estou aprendendo JavaScript às 23:12 do dia 07/02/2025.')
+console.log('Meu nome é Mateus. Estou aprendendo JavaScript às', 23,  'do dia 07/02/2025.')
