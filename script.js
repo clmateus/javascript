ce6333c7369a2af9eb7786e9559617fbd04bd8c4
clmateus <cl.mateuscosta@gmail.com>
2025-02-07 23:28:21 -0300

    script.js updated

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // console.log
 console.log("hello world")
 console.log("teste")
+
+// variáveis
+let nome = "João"
+console.log(nome, " nasceu em 1984.")
+const nome2 = "Maria"
+console.log(nome2, " nasceu em 1985.")
