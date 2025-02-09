6584d26be15cabd8f484ac9e7ee191658e3e0cb9
clmateus <cl.mateuscosta@gmail.com>
2025-02-08 23:09:08 -0300

    aritmetic operations

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ let nome = "João"
 console.log(nome, " nasceu em 1984.")
 const nome2 = "Maria"
 console.log(nome2, " nasceu em 1985.")
+
+// operaçõs aritméticas
+console.log(1+1)
+console.log(1-1)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**2)
