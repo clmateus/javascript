ada085653f274e1cdb82bacd1b255be47b8570d3
clmateus <cl.mateuscosta@gmail.com>
2025-02-09 00:39:56 -0300

    added exercicio4.js

--- exercicio3.js
+++ exercicio3.js
@@ -1,4 +1,4 @@
-const num1 = prompt("Digite um número")
-const num2 = prompt("Digite outro número")
+const num1 = Number(prompt("Digite um número"))
+const num2 = Number(prompt("Digite outro número"))
 const result = num1 + num2
 alert(result)
---
+++ exercicio4.js
@@ -0,0 +1,5 @@
+let varA = varB
+let varB = varC
+let varC = varA
+
+console.log(varA, varB, varC)
