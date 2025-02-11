05f3f328e00955ea074b94e12409a2e98c5a9221
clmateus <cl.mateuscosta@gmail.com>
2025-02-10 22:44:39 -0300

    string update

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // console.log
-console.log("hello world")
-console.log("teste")
+// console.log("hello world")
+// console.log("teste")
 
 // variáveis
-let nome = "João"
-console.log(nome, " nasceu em 1984.")
-const nome2 = "Maria"
-console.log(nome2, " nasceu em 1985.")
+// let nome = "João"
+// console.log(nome, " nasceu em 1984.")
+// const nome2 = "Maria"
+// console.log(nome2, " nasceu em 1985.")
 
 // operaçõs aritméticas
-console.log(1+1)
-console.log(1-1)
-console.log(2*2)
-console.log(2/2)
-console.log(2%2)
-console.log(2**2)
-console.log(1++)
-console.log(1--)
+// console.log(1+1)
+// console.log(1-1)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+// console.log(1++)
+// console.log(1--)
 
 // alert, confirm, prompt
-alert("teste")
-let confirma = confirm("teste")
-prompt("teste")
+// alert("teste")
+// let confirma = confirm("teste")
+// prompt("teste")
 
 // strings
 let umaString = "Um texto"
+console.log(umaString[4])
+console.log(umaString.charAt(6))
