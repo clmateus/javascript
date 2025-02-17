02b2316a41e8d27cf78f9641434bdf56e87c1700
clmateus <cl.mateuscosta@gmail.com>
2025-02-17 18:25:58 -0300

    object update

--- script.js
+++ script.js
@@ -80,7 +80,14 @@
 // console.log(soma(2, 2))
 // console.log(soma(5, 5))
 // console.log(soma(3, 3))
-const raiz = function (n) {
-    return n ** 0.5;
-};
-console.log(raiz(9))
+// const raiz = (n) => n ** 0.5;
+// console.log(raiz(9))
+// console.log(raiz(16))
+// console.log(raiz(25))
+
+// Objetos
+const pessoa = {
+    nome: 'Mateus',
+    idade: 24
+}
+console.log(pessoa)
