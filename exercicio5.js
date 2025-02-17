978283cc63e64bff642eaad61b23795b5a990497
clmateus <cl.mateuscosta@gmail.com>
2025-02-17 20:12:35 -0300

    exercicio5.js finished

--- exercicio5.js
+++ exercicio5.js
@@ -1,21 +1,32 @@
-const form = document.querySelector("#form")
-const nome = form.querySelector('#nome')
-const sobrenome = form.querySelector('#sobrenome')
-const peso = form.querySelector('#peso')
-const altura = form.querySelector('#altura')
-const resultado = document.querySelector('.resultado')
+function escopo() {
+    const form = document.querySelector("#form")
+    const pessoas = []
+
+    function recebeEventoForm(e) {
+        e.preventDefault()
+
+        const nome = form.querySelector('#nome')
+        const sobrenome = form.querySelector('#sobrenome')
+        const peso = form.querySelector('#peso')
+        const altura = form.querySelector('#altura')
+        const resultado = document.querySelector('.resultado')
+
+        pessoas.push({nome:nome.value, sobrenome:sobrenome.value, peso:peso.value, altura:altura.value})
+        console.log(pessoas)
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
+    }
+
+    form.addEventListener('submit', recebeEventoForm)
+
+}
+
+escopo()
 
 // form.onsubmit = function(e) {
 //     e.preventDefault()
 // }
 
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-})
-
-function criaPessoa() {
-    return {nome: nome, sobrenome: sobrenome, peso: peso, altura: altura}
-
-}
-
-console.log(nome)
+// form.addEventListener('submit', function(e){
+//     e.preventDefault()
+// })
